eaec01163ca8489c0739a87cc23b45e5bab6e5f3
FernandoAvakian <152490088+FernandoAvakian@users.noreply.github.com>
2024-08-29 14:34:12 +0200

    ComplAPI

--- webpack.production.js
+++ webpack.production.js
@@ -26,4 +26,10 @@ module.exports = merge(common, {
     }),
     new CompressionPlugin(),
   ],
+  new webpack.DefinePlugin({
+    'process.env': {
+      REACT_APP_PLANET_API_KEY: JSON.stringify(process.env.REACT_APP_PLANET_API_KEY),
+      REACT_APP_GFW_DATA_API_KEY: JSON.stringify(process.env.REACT_APP_GFW_DATA_API_KEY),
+    },
+  }),
 });

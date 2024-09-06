18261149d1e078bf0fd05e9a90d2bce42ccaf9d6
FernandoAvakian <152490088+FernandoAvakian@users.noreply.github.com>
2024-09-06 09:22:21 +0200

    API

--- src/js/components/leftPanel/analysisPanel/analysisUtils.ts
+++ src/js/components/leftPanel/analysisPanel/analysisUtils.ts
@@ -127,6 +127,6 @@ export function generateWidgetURL({
   if (analysisId === 'LCC' || analysisId === 'TC_GAIN_TOTAL') {
     baseURL = baseURL.concat(`&sql=${sqlString}`);
   }
-  const key = '7abb4a7b-646f-4a9f-8ee6-48d76c37782';
+  const key = '7abb4a7b-646f-4a9f-8ee6-48d76c37782c';
   return `${baseURL}&x-api-key=${key}`;
 }
